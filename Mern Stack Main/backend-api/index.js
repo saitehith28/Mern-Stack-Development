2b05522c1672ee91f950f1653243920ecbd929a1
Tehith <saitehith028@gmail.com>
2022-12-17 23:38:37 +0530

    api commited

--- Mern Stack Main/backend-api/index.js
+++ Mern Stack Main/backend-api/index.js
@@ -1,66 +1,13 @@
 var express=require("express");
-const bodyParser=require("body-parser");
+var bodyParser=require("body-parser");
 var app=express();
 var port=5000;
 
 var users=require("./routes/users");
-const { json } = require("express");
 var jsonParser=bodyParser.json();
-app.use(bodyParser,json({type:'application/json'}));
+app.use(bodyParser.json({type:'application/*+json'}));
 
 app.use("/users",jsonParser,users);
 app.listen(port,function(){
-    console.log(`Server running on ${port}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function checkRoles(req,res,next){
-//     console.log("I am middleware");
-//     res.send("You dont have access");
-//     next();
-// }
-// app.use(checkRoles)
-
-// router.use(function(req,res,next){
-//     console.log("Hello I am getting routes");
-//     res.send("I am routes");
-//     next();
-// })
-// app.use("/user",router)
-
-// app.get("/",function(req,res){
-//     res.send("Hello from express.js");
-// });
-
-// app.get("/student",function(req,res){
-//     res.send("<html><body><h1>Hello from student</h1></body></html>");
-// })
-
-// app.get("/user",function(req,res){
-//     res.send({id:1,name:"Tehith"});
-// })
+    console.log(`Server running at ${port}`);
+})
--- Mern Stack Main/backend-api/routes/users.js
+++ Mern Stack Main/backend-api/routes/users.js
@@ -2,17 +2,43 @@ var express=require("express");
 var router=express.Router();
 var users=[
     {
+        id:1,
         name:"Tehith",
-        age:30,
+        age:20,
         phone:9121645842
     }
 ]
+
 router.get("/",function(req,res){
-    res.send("I am user router")
+    res.send(users);
 })
+
 router.post("/create",function(req,res){
-    console.log(req.body);
-    const {name,age,phone}=req.body;
-    users.push({name,age,phone});
+    console.log("Req=",req,"Reqparams=",req.params)
+    const {id,name,age,phone}=req.body;
+    users.push({id,name,age,phone});
+    res.send("User Created Successfully");
 })
+
+router.delete("/:id/delete",function(req,res){
+    console.log("Reqparams",req.params);
+    let filteredUsers=users.filter(function(item){
+        return item.id!=Number(req.params.id);
+    });
+    users=[...filteredUsers];
+    res.send("User deleted successfully");
+})
+
+router.put("/:id/update",function(req,res){
+    var i;
+    console.log(req,res)
+    for(i of users){
+        if(i.id==Number(req.params.id)){
+            console.log(i)
+            i.name="Taduka Sai Tehith"
+        }
+    }
+    res.send("User Updated Successfully");
+})
+
 module.exports=router;
